fea02783befe5e3fdcf0094dd6fd9d1cdb91b62a
techcoder2007 <techcoder444@gmail.com>
2024-09-10 02:25:21 +0500

    added product details modal

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -3,9 +3,11 @@ import "toastify-js/src/toastify.css";
 
 import { adminPasswords } from "../data/login-schema.json";
 import { wait } from "../utils/wait";
+
 const loginForm = document.querySelector(".login-form");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
+const loginBtn = document.querySelector("#loginBtn");
 
 loginForm.addEventListener("submit", function (event) {
   event?.preventDefault();
@@ -37,6 +39,11 @@ loginForm.addEventListener("submit", function (event) {
     backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
     stopOnFocus: true,
   }).showToast();
+  
+
+  loginBtn.disabled = true;
+  loginBtn.style.opacity = "0.5";
+  
 
   wait(2000).then(() => {
     window.location.href = "/";
